46d9654173577ba645b3f1be4169b6df14068a96
Steven Kelly <114993881+StevenKelly03@users.noreply.github.com>
2025-09-17 16:48:24 +0100

    Fixed error in reverse test, started on Sprint 2

--- src/lib/filters/reverse.test.ts
+++ src/lib/filters/reverse.test.ts
@@ -3,12 +3,12 @@ import { reverseInput } from './reverse';
 
 describe('reverseInput function', () => {
   it('reverses a string of numbers separated by spaces', () => { 
-    const input = "4 3 10 1 127"; 
+    const input = "4,3,10,1,127"; 
     const expected = "127 1 10 3 4";
     expect(reverseInput(input)).toEqual(expected);
   });
   it('reverses multiple words', () => {
-    const input = "the quick brown fox";
+    const input = "fox brown quick the";
     const expected = "fox brown quick the";
     expect(reverseInput(input)).toEqual(expected);
   });
