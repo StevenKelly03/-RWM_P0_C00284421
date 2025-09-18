6ee248677b3a0ad665e077b44f848e98e060fc9f
Steven Kelly <114993881+StevenKelly03@users.noreply.github.com>
2025-09-18 13:07:19 +0100

    finally fixed

--- src/lib/filters/combined/combined.ts
+++ src/lib/filters/combined/combined.ts
@@ -1,5 +1,5 @@
-import { facts as peerFacts } from 'realwsim'
 import { reverseInput as myReverse } from '../reverse'
+import { facts as peerFacts } from 'realwsim/src/lib/filters'
 
 export function Combined(xs: number[]): string {
     // Apply factorial, then reverse order as a string
