afddc54e870e7580ee23c0f332b19ad4f192fd2b
Steven Kelly <114993881+StevenKelly03@users.noreply.github.com>
2025-10-13 12:03:09 +0100

    Added modified Cube filter

---
+++ Sprint 7/src/lib/filters/cube.ts
@@ -0,0 +1,9 @@
+// Pure function: cube a single integer
+export function cube(x: number): number {
+  return Math.pow(x, 3);
+}
+
+// Map over a sequence
+export function cubes(xs: number[]): number[] {
+  return xs.map((value) => cube(value));
+}
