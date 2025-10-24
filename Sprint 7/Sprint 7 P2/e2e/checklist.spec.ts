06b6ca0570bcdfbdda502ba1c3872343a85ee66f
Danny <116danny.delaney@gmail.com>
2025-10-24 17:28:24 +0100

    submodule added

--- Sprint 7/Sprint 7 P2/e2e/checklist.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('checklist page has title', async ({ page }) => 
-{
-  await page.goto('/lab/checklist');
-  const title = page.getByTestId('title');
-  await expect(title).toHaveText('Progress Checklist');
-});
--- Sprint 7/Sprint 7 P2/src/routes/lab/checklist/+page.svelte
+++
@@ -1,7 +0,0 @@
-<h1 data-testid="title">Progress Checklist</h1>
-
-<script>
-  import ChecklistProgress from '$lib/ChecklistProgress.svelte';
-</script>
-
-<ChecklistProgress />
